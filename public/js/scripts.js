361e92a247eb9cfe4f008fcb766dfc0b2c69d09a
Luis Fernando Rodríguez <lfrodriguez@lightsoft.com.mx>
2023-07-13 12:45:45 -0600

    test
    test

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -19,7 +19,7 @@
 		// Create a countdown instance. Change the launchDay according to your needs.
 		// The month ranges from 0 to 11. I specify the month from 1 to 12 and manually subtract the 1.
 		// Thus the launchDay below denotes 7 October, 2017.
-		var launchDay = new Date(2017, 10-1, 7);
+		var launchDay = new Date(2023, 7-1, 29);
 		$('#timer').countdown({
 			until: launchDay
 		});
